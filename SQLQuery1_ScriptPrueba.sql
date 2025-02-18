53ba7b6f5166f651215b9174a0aa1b60139706bc
Paula Nyszta <paula4nyszta@gmail.com>
2025-02-18 20:34:38 -0300

    arreglo de prueba

--- SQLQuery1_ScriptPrueba.sql
+++ SQLQuery1_ScriptPrueba.sql
@@ -13,7 +13,7 @@ EXEC ddbba.insertarSucursal
 SELECT * FROM ddbba.Sucursal WHERE id_sucursal = 90000; --observe que la sucursal fue agregado exitosamente
 
 
---prueba para SP insertarEmpleado, Ejecute los siguientes juegos de prueba
+--prueba para SP insertarEmpleado, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarEmpleado
     11111,'2030-01-01','11-11111111-','Av','Gomez','Pedro','GP@','GP@','T','Sup'; --debe dar error el id
@@ -29,7 +29,7 @@ EXEC ddbba.insertarEmpleado
     900000,'2019-01-01','11-11111111-1','Av','Gomez','Pedro','GP@','GP@','TT','Sup'; --debe dar error en id
 SELECT * FROM ddbba.Empleado WHERE id_empleado = 900000; --observe que el empleado fue agregado exitosamente
 
---prueba para SP insertarProveedor, Ejecute los siguientes juegos de prueba
+--prueba para SP insertarProveedor, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarProveedor
 	90000,''; --debe dar error el nombre
@@ -39,7 +39,7 @@ EXEC ddbba.insertarProveedor
 	90000,'Jose'; --debe dar error el id
 SELECT * FROM ddbba.Proveedor WHERE id_proveedor = 90000; --observe que el proveedor fue agregado exitosamente
 
---prueba para SP insertarProducto, Ejecute los siguientes juegos de prueba
+--prueba para SP insertarProducto, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarProducto
 	0,-1,'','' --debe dar error el id
@@ -55,7 +55,7 @@ EXEC ddbba.insertarProducto
 	90000,1,'bebida','muy rica' --debe dar error el id
 SELECT * FROM ddbba.Producto WHERE id_producto = 90000 --observe que el producto fue agregado exitosamente
 
--- prueba para SP insertarProvee, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarProvee, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarProvee
 	10,10 --debe dar error el proveedor
@@ -66,7 +66,7 @@ EXEC ddbba.insertarProvee
 SELECT * FROM ddbba.Provee WHERE id_producto = 90000 AND id_proveedor = 90000; --observe que los datos fueron agregado exitosamente
 
 
---prueba para SP insertarCliente, Ejecute los siguientes juegos de prueba
+--prueba para SP insertarCliente, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarCliente
     -1,'Mal','Norma','Gomez','Pedro','2030-11-12'; --debe dar error el id
@@ -82,7 +82,7 @@ EXEC ddbba.insertarCliente
     90000,'Male','Normal','Gomez','Pedro','2019-11-12'; --debe decir que el id ya existe
 SELECT * FROM ddbba.Cliente WHERE id_cliente = 90000 --observe que el cliente fue agregado exitosamente
 
--- prueba para SP insertarMedioPago, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarMedioPago, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.InsertarMedioPago
 	-1,'' ;--debe dar error el id
@@ -94,7 +94,7 @@ EXEC ddbba.InsertarMedioPago
 	90000,'' ;--debe dar un id ya existente
 SELECT * FROM ddbba.MedioPago WHERE id_mp = 90000 --observe que el medio de pago fue agregado exitosamente
 
--- prueba para SP insertarPedido, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarPedido, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.InsertarPedido
 	-1,'2030-01-02','00:00',90000,90000,''; --debe dar error en el id
@@ -106,7 +106,7 @@ EXEC ddbba.InsertarPedido
 	90000,'2020-01-02','00:00',90000,90000,'111111111'; --debe ingresarse los datos exitosamente
 SELECT * FROM ddbba.Pedido WHERE id_pedido = 90000 --observe que el pedido fue agregado exitosamente
 
--- prueba para SP insertarVenta, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarVenta, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarVenta
 	10,10,10; --debe dar error el id de pedido
@@ -119,7 +119,7 @@ EXEC ddbba.insertarVenta
 SELECT * FROM ddbba.Venta WHERE id_pedido = 90000 AND id_sucursal = 90000 AND id_empleado = 900000; --observe que la venta fue agregado exitosamente
 
 
--- prueba para SP insertarTiene, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarTiene, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarTiene
 	10,10,0; --debe dar error el id de producto
@@ -132,7 +132,7 @@ EXEC ddbba.insertarTiene
 SELECT * FROM ddbba.Tiene WHERE id_pedido = 90000 AND id_producto = 90000 ;--observe que los datos fueron agregado exitosamente
 
 
--- prueba para SP insertarFactura, Ejecute los siguientes juegos de prueba
+-- prueba para SP insertarFactura, Ejecute los siguientes juegos de prueba y  luego el SELECT para ver los resultados
 -- datos invalidos
 EXEC ddbba.insertarFactura
 	'1','Z',10,'2030-01-01'; --debe dar error en el id
@@ -150,7 +150,7 @@ SELECT * FROM ddbba.Factura WHERE id_factura = '900-00-0000' --observe que la fa
 
 
 
-
+--Luego de ejecutar los SP, emilinar los datos de prueba de las tablas
 DELETE FROM ddbba.Sucursal WHERE id_sucursal = 90000;
 DELETE FROM ddbba.Empleado WHERE id_empleado = 900000;
 DELETE FROM ddbba.Proveedor WHERE id_proveedor = 90000;
@@ -164,18 +164,4 @@ DELETE FROM ddbba.Tiene WHERE id_pedido= 90000 AND id_producto = 90000;
 DELETE FROM ddbba.Factura WHERE id_factura= '900-00-0000' ;
 
 
-
-
---borrar tablas
- DROP TABLE ddbba.Factura
-DROP TABLE ddbba.Tiene
-DROP TABLE ddbba.Venta
-DROP TABLE ddbba.Pedido
-DROP TABLE ddbba.MedioPago
-DROP TABLE ddbba.Cliente
-DROP TABLE ddbba.Provee
-DROP TABLE ddbba.Producto
-DROP TABLE ddbba.Proveedor
-DROP TABLE ddbba.Empleado
-DROP TABLE ddbba.Sucursal
 
